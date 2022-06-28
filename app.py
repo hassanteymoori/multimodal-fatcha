8d688a078451ddd2adce5afac91a89c8427e2a4c
Hassan Teymoori <hassan.teymoori1372@gmail.com>
2022-06-28 11:34:44 +0200

    update app.py

--- app.py
+++ app.py
@@ -12,16 +12,25 @@ key_points_classifier = KeyPointClassifier()
 while cap.isOpened():
     # Read the frame
     success, frame = cap.read()
-    # frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-    # frame_height, frame_width = frame.shape[:2]
+
     if not success:
         print("Ignoring empty camera frame.")
         # If loading a video, use 'break' instead of 'continue'.
         continue
 
-    face_detected_frame = face_mesh.detect(cv2.flip(frame, 1), True)
-    hand_face_detected_frame, results = hand_landmarks.detect(face_detected_frame, True, landmarks=True)
-    hand_face_gesture_frame = key_points_classifier.show_on_screen(hand_face_detected_frame, results)
+    face_detected_frame, head_pose_class = face_mesh.detect(
+        cv2.flip(frame, 1),
+        with_pose_estimator=True
+    )
+    hand_face_detected_frame, results, hand_class_label = hand_landmarks.detect(
+        face_detected_frame,
+        draw_bbox=False,
+        landmarks=True
+    )
+    hand_face_gesture_frame, gesture_class_id = key_points_classifier.show_on_screen(
+        hand_face_detected_frame,
+        results
+    )
 
     cv2.imshow('img', hand_face_gesture_frame)
     # Stop if escape key is pressed
